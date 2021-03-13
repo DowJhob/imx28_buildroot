b656b37e3db9acc624aa8dc0a907c643571dd24e
DowJhob <eulle@yandex.ru>
2021-03-13 16:04:25 +0700

    libusb to 24, many more update

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -5,6 +5,11 @@
 ################################################################################
 
 ifeq ($(BR2_PACKAGE_QT5_VERSION_LATEST),y)
+QT5_VERSION_MAJOR = 5.15
+QT5_VERSION = $(QT5_VERSION_MAJOR).0
+QT5_SOURCE_TARBALL_PREFIX = everywhere-src
+endif
+ifeq ($(BR2_PACKAGE_QT5_VERSION_5_12),y)
 QT5_VERSION_MAJOR = 5.12
 QT5_VERSION = $(QT5_VERSION_MAJOR).4
 QT5_SOURCE_TARBALL_PREFIX = everywhere-src
@@ -13,7 +18,7 @@ QT5_VERSION_MAJOR = 5.6
 QT5_VERSION = $(QT5_VERSION_MAJOR).3
 QT5_SOURCE_TARBALL_PREFIX = opensource-src
 endif
-QT5_SITE = https://download.qt.io/archive/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
+QT5_SITE = https://download.qt.io/official_releases/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
 
 include $(sort $(wildcard package/qt5/*/*.mk))
 
